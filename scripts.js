90496078c14cb8ad421cc4c51163af8abd8598a6
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-10 23:52:00 +0200

    added map method to convert provinces to uppercase and log

--- scripts.js
+++ scripts.js
@@ -15,10 +15,18 @@ const products = [
 ]
 
 
+/* ForEach Exercise */
+// Loop through each name and log it with an emoji and index
 names.forEach((name, index) => console.log(`Name ${index + 1}: --> 👤${name}`));
 
+// Loop through each province and log it with an emoji and index
 provinces.forEach((province, index) => console.log(`Province ${index + 1}: 🌍 ${province}`));
 
+// Loop through each name and log it with its matching province using emojis
 names.forEach((name, index) => {
   console.log(`👤 ${name} 🌍 (${provinces[index]})`);
 });
+
+/* Map Method Exercise */
+const provincesInUpperCase = provinces.map(province => province.toUpperCase());
+console.log(provincesInUpperCase);
